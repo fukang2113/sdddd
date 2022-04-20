9e95710c27dee96cfb27902546af994d8ae32cb4
fukang2113 <18844549167@163.com>
2022-04-20 17:46:35 +0800

    uploaded artifacts by fukang2113 on Wed, 20 Apr 2022 17:46:31 +0800

--- ss/grpc-go/ssd/sd/Aqq.pb.gw.go
+++ ss/grpc-go/ssd/sd/Aqq.pb.gw.go
@@ -31,24 +31,43 @@ var _ = runtime.String
 var _ = utilities.NewDoubleArray
 var _ = metadata.Join
 
-func request_Aqq_SayHello_0(ctx context.Context, marshaler runtime.Marshaler, client AqqClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq HelloRequest
+var (
+	filter_Aqq_Heathcheck_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
+func request_Aqq_Heathcheck_0(ctx context.Context, marshaler runtime.Marshaler, client AqqClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq HealthcheckRequest
 	var metadata runtime.ServerMetadata
 
-	newReader, berr := utilities.IOReaderFactory(req.Body)
-	if berr != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Aqq_Heathcheck_0); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
-	msg, err := client.SayHello(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	msg, err := client.Heathcheck(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
 
 }
 
-func local_request_Aqq_SayHello_0(ctx context.Context, marshaler runtime.Marshaler, server AqqServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+func local_request_Aqq_Heathcheck_0(ctx context.Context, marshaler runtime.Marshaler, server AqqServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq HealthcheckRequest
+	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Aqq_Heathcheck_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.Heathcheck(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
+func request_Aqq_Helloworld_0(ctx context.Context, marshaler runtime.Marshaler, client AqqClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq HelloRequest
 	var metadata runtime.ServerMetadata
 
@@ -60,7 +79,24 @@ func local_request_Aqq_SayHello_0(ctx context.Context, marshaler runtime.Marshal
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
-	msg, err := server.SayHello(ctx, &protoReq)
+	msg, err := client.Helloworld(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_Aqq_Helloworld_0(ctx context.Context, marshaler runtime.Marshaler, server AqqServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq HelloRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.Helloworld(ctx, &protoReq)
 	return msg, metadata, err
 
 }
@@ -71,19 +107,19 @@ func local_request_Aqq_SayHello_0(ctx context.Context, marshaler runtime.Marshal
 // Note that using this registration option will cause many gRPC library features to stop working. Consider using RegisterAqqHandlerFromEndpoint instead.
 func RegisterAqqHandlerServer(ctx context.Context, mux *runtime.ServeMux, server AqqServer) error {
 
-	mux.Handle("POST", pattern_Aqq_SayHello_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("GET", pattern_Aqq_Heathcheck_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		var stream runtime.ServerTransportStream
 		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
 		var err error
-		ctx, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/ssd.sd.Aqq/SayHello", runtime.WithHTTPPathPattern("/ssd.sd.Aqq/SayHello"))
+		ctx, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/ssd.sd.Aqq/Heathcheck", runtime.WithHTTPPathPattern("/api/healthcheck"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := local_request_Aqq_SayHello_0(ctx, inboundMarshaler, server, req, pathParams)
+		resp, md, err := local_request_Aqq_Heathcheck_0(ctx, inboundMarshaler, server, req, pathParams)
 		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
@@ -91,7 +127,31 @@ func RegisterAqqHandlerServer(ctx context.Context, mux *runtime.ServeMux, server
 			return
 		}
 
-		forward_Aqq_SayHello_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+		forward_Aqq_Heathcheck_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("POST", pattern_Aqq_Helloworld_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		ctx, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/ssd.sd.Aqq/Helloworld", runtime.WithHTTPPathPattern("/api/helloworld"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_Aqq_Helloworld_0(ctx, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Aqq_Helloworld_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -136,24 +196,45 @@ func RegisterAqqHandler(ctx context.Context, mux *runtime.ServeMux, conn *grpc.C
 // "AqqClient" to call the correct interceptors.
 func RegisterAqqHandlerClient(ctx context.Context, mux *runtime.ServeMux, client AqqClient) error {
 
-	mux.Handle("POST", pattern_Aqq_SayHello_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("GET", pattern_Aqq_Heathcheck_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
 		var err error
-		ctx, err = runtime.AnnotateContext(ctx, mux, req, "/ssd.sd.Aqq/SayHello", runtime.WithHTTPPathPattern("/ssd.sd.Aqq/SayHello"))
+		ctx, err = runtime.AnnotateContext(ctx, mux, req, "/ssd.sd.Aqq/Heathcheck", runtime.WithHTTPPathPattern("/api/healthcheck"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := request_Aqq_SayHello_0(ctx, inboundMarshaler, client, req, pathParams)
+		resp, md, err := request_Aqq_Heathcheck_0(ctx, inboundMarshaler, client, req, pathParams)
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
 
-		forward_Aqq_SayHello_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+		forward_Aqq_Heathcheck_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("POST", pattern_Aqq_Helloworld_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		ctx, err = runtime.AnnotateContext(ctx, mux, req, "/ssd.sd.Aqq/Helloworld", runtime.WithHTTPPathPattern("/api/helloworld"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_Aqq_Helloworld_0(ctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Aqq_Helloworld_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -161,9 +242,13 @@ func RegisterAqqHandlerClient(ctx context.Context, mux *runtime.ServeMux, client
 }
 
 var (
-	pattern_Aqq_SayHello_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"ssd.sd.Aqq", "SayHello"}, ""))
+	pattern_Aqq_Heathcheck_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"api", "healthcheck"}, ""))
+
+	pattern_Aqq_Helloworld_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"api", "helloworld"}, ""))
 )
 
 var (
-	forward_Aqq_SayHello_0 = runtime.ForwardResponseMessage
+	forward_Aqq_Heathcheck_0 = runtime.ForwardResponseMessage
+
+	forward_Aqq_Helloworld_0 = runtime.ForwardResponseMessage
 )
--- ss/grpc-go/ssd/sd/Aqq.pb.validate.go
+++ ss/grpc-go/ssd/sd/Aqq.pb.validate.go
@@ -35,6 +35,214 @@ var (
 	_ = sort.Sort
 )
 
+// Validate checks the field values on HealthcheckRequest with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the first error encountered is returned, or nil if there are no violations.
+func (m *HealthcheckRequest) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on HealthcheckRequest with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the result is a list of violation errors wrapped in
+// HealthcheckRequestMultiError, or nil if none found.
+func (m *HealthcheckRequest) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *HealthcheckRequest) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	// no validation rules for Msg
+
+	if len(errors) > 0 {
+		return HealthcheckRequestMultiError(errors)
+	}
+
+	return nil
+}
+
+// HealthcheckRequestMultiError is an error wrapping multiple validation errors
+// returned by HealthcheckRequest.ValidateAll() if the designated constraints
+// aren't met.
+type HealthcheckRequestMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m HealthcheckRequestMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m HealthcheckRequestMultiError) AllErrors() []error { return m }
+
+// HealthcheckRequestValidationError is the validation error returned by
+// HealthcheckRequest.Validate if the designated constraints aren't met.
+type HealthcheckRequestValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e HealthcheckRequestValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e HealthcheckRequestValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e HealthcheckRequestValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e HealthcheckRequestValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e HealthcheckRequestValidationError) ErrorName() string {
+	return "HealthcheckRequestValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e HealthcheckRequestValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sHealthcheckRequest.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = HealthcheckRequestValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = HealthcheckRequestValidationError{}
+
+// Validate checks the field values on HealthcheckResponse with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the first error encountered is returned, or nil if there are no violations.
+func (m *HealthcheckResponse) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on HealthcheckResponse with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the result is a list of violation errors wrapped in
+// HealthcheckResponseMultiError, or nil if none found.
+func (m *HealthcheckResponse) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *HealthcheckResponse) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	// no validation rules for Result
+
+	if len(errors) > 0 {
+		return HealthcheckResponseMultiError(errors)
+	}
+
+	return nil
+}
+
+// HealthcheckResponseMultiError is an error wrapping multiple validation
+// errors returned by HealthcheckResponse.ValidateAll() if the designated
+// constraints aren't met.
+type HealthcheckResponseMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m HealthcheckResponseMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m HealthcheckResponseMultiError) AllErrors() []error { return m }
+
+// HealthcheckResponseValidationError is the validation error returned by
+// HealthcheckResponse.Validate if the designated constraints aren't met.
+type HealthcheckResponseValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e HealthcheckResponseValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e HealthcheckResponseValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e HealthcheckResponseValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e HealthcheckResponseValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e HealthcheckResponseValidationError) ErrorName() string {
+	return "HealthcheckResponseValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e HealthcheckResponseValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sHealthcheckResponse.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = HealthcheckResponseValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = HealthcheckResponseValidationError{}
+
 // Validate checks the field values on HelloRequest with the rules defined in
 // the proto definition for this message. If any rules are violated, the first
 // error encountered is returned, or nil if there are no violations.
@@ -57,7 +265,16 @@ func (m *HelloRequest) validate(all bool) error {
 
 	var errors []error
 
-	// no validation rules for Msg
+	if utf8.RuneCountInString(m.GetMsg()) < 3 {
+		err := HelloRequestValidationError{
+			field:  "Msg",
+			reason: "value length must be at least 3 runes",
+		}
+		if !all {
+			return err
+		}
+		errors = append(errors, err)
+	}
 
 	if len(errors) > 0 {
 		return HelloRequestMultiError(errors)
